3e0b4d7dcd21d31e255d4ab9a3432122d5ee1b3d
cresc28 <crescendo010828@gmail.com>
2025-09-23 13:49:20 +0900

    パスを変更

--- HoshiidesuBot/sheet_reader.py
+++ HoshiidesuBot/sheet_reader.py
@@ -154,9 +154,8 @@ class GSheetReader(commands.Cog):
 
         def play_audio(file_name):
             path = f"HoshiidesuBot/audio/{file_name}"
-            print(f"再生試行: {path}, 存在={os.path.exists(path)}")
             try:
-                source = discord.FFmpegPCMAudio(executable="ffmpeg", source=path)
+                source = discord.FFmpegPCMAudio(executable="/usr/bin/ffmpeg", source=path)
                 voice_client.play(source)
             except Exception as e:
                 print(f"音声再生エラー: {e}")
